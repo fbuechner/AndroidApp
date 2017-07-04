e262557f13ec25081a0f739e853f2f5608afdb5c
KONI924 <lars.konusch@gmx.de>
2017-07-04 14:11:18 +0200

    erste Schritte

---
+++ app/src/main/java/com/example/lkonusch/testapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lkonusch.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // called on button press
+    public void sendMessage(View view) {
+
+    }
+}
