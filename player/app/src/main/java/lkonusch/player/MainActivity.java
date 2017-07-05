a52b4c6d1ee4b6133b53eb0a9c022f6ce9d35826
KONI924 <lars.konusch@gmx.de>
2017-07-05 12:44:43 +0200

    nextTry

---
+++ player/app/src/main/java/lkonusch/player/MainActivity.java
@@ -0,0 +1,61 @@
+package lkonusch.player;
+
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Song> songList;
+    private ListView songView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // retrieve ListView instance using ID from main layout
+        songView = (ListView)findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
+        getSongList();
+        // sort songs by title
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+        SongAdapter songAdt = new SongAdapter(this, songList);
+        songView.setAdapter(songAdt);
+    }
+
+    public void getSongList() {
+        // retrieve song info
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        // check data
+        if(musicCursor != null && musicCursor.moveToFirst()) {
+            // get columns
+            int titleColumn     = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn    = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.ARTIST);
+            // add songs to list
+            do {
+                long thisID         = musicCursor.getLong(idColumn);
+                String thisTitle    = musicCursor.getString(titleColumn);
+                String thisArtist   = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisID, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+        }
+    }
+}
